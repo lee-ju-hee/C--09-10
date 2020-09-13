e28ceba2760651b12d94b5578b71a56729e6222b
lee-ju-hee <55524173+lee-ju-hee@users.noreply.github.com>
2020-09-13 17:54:37 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Project_09_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("학번: 201987040");
+            Console.WriteLine("이름: 이주희");
+        }
+    }
+}
